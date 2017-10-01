f4d38d5f3af531f9eba6a58eb4bae8f219d8d5e9
Tim Robinson <timrobinson41@gmail.com>
2017-10-01 14:52:12 -0700

    Added Example 9
    Added the Example9 folder and updated the book to cover the new code.

---
+++ Source code for book/Chapter2/Example01/BugExplorer/BugExplorer/Areas/HelpPage/ModelDescriptions/EnumTypeModelDescription.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace BugExplorer.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumTypeModelDescription : ModelDescription
+    {
+        public EnumTypeModelDescription()
+        {
+            Values = new Collection<EnumValueDescription>();
+        }
+
+        public Collection<EnumValueDescription> Values { get; private set; }
+    }
+}
---
+++ Source code for book/Chapter2/Example01/BugExplorer/BugExplorer/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BugExplorer.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
