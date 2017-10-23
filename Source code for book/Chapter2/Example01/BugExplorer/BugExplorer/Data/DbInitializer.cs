52050ffeb75c6757ccc6521a0847f968dee5328e
Tim Robinson <timrobinson41@gmail.com>
2017-10-23 12:05:02 -0700

    Added controllers and views
    Added controllers and views for db tables Bugs, Observations, and Photos. Added images folder.

--- Source code for book/Chapter2/Example01/BugExplorer/BugExplorer/Data/DbInitializer.cs
+++ Source code for book/Chapter2/Example01/BugExplorer/BugExplorer/Data/DbInitializer.cs
@@ -20,39 +20,39 @@ namespace BugExplorer.Data
             var bugs = new Bug[]
             {
                 new Bug{ID=1000, CommonName="Tarantula Hawk Wasp", Class="Insecta", Order="Hymenoptera", Family="Pompilidae", Genus="Pepsis", Species="", Description="The tarantula hawk is an example of a parasitoid wasp. It will sting a taratula to paralyse it, then drag the spider to a brood chamber, where it lays a single egg on the spider's abdomen. When the egg hatches, the wasp larva begins eating the spider -- slowly. It actually eats the non-vital organs first to keep it alive for as long as possible!"},
-                new Bug{ID=1001, CommonName="Wolf Spider", Class="Arachnida", Order="Araneae", Family="Lycosidae", Genus="", Species="", Description="Wolf spiders carry their babies on their backs. They're the only spiders that do this."}
+                new Bug{ID=1001, CommonName="Wolf Spider", Class="Arachnida", Order="Araneae", Family="Lycosidae", Genus="", Species="", Description="Wolf spiders carry their babies on their backs. They're the only spiders that do this."},
             };
-            foreach (Bug s in bugs)
+            foreach (Bug b in bugs)
             {
-                context.Bugs.Add(s);
+                context.Bugs.Add(b);
             }
             context.SaveChanges();
 
             var observations = new Observation[]
             {
-                new Observation{ID=2000, Title="Wolf Spider with Babies", BugID=2,Date=DateTime.Parse("2017-04-12")},
-                new Observation{ID=2001, Title="Tarantula Hawk Dragging Tarantula", BugID=1,Date=DateTime.Parse("2017-09-08")},
-                new Observation{ID=2002, Title="Wolf Spider with Missing Legs", BugID=2,Date=DateTime.Parse("2017-08-31")}
+                new Observation{ID=2000, Title="Wolf Spider in Burrow", BugID=1001, Date=DateTime.Parse("2017-04-12")},
+                new Observation{ID=2001, Title="Tarantula Hawk Dragging Tarantula", BugID=1000, Date=DateTime.Parse("2017-09-08")},
+                new Observation{ID=2002, Title="Wolf Spider with Missing Legs", BugID=1001, Date=DateTime.Parse("2017-08-31")}
             };
-            foreach (Observation c in observations)
+            foreach (Observation o in observations)
             {
-                context.Observations.Add(c);
+                context.Observations.Add(o);
             }
             context.SaveChanges();
 
-            Guid id1 = Guid.NewGuid();
-            Guid id2 = Guid.NewGuid();
-            Guid id3 = Guid.NewGuid();
+            Guid id1 = Guid.Parse("e1335dba-00d3-48ce-a9a2-56a5b03a4384");
+            Guid id2 = Guid.Parse("039f9a32-572b-4f6b-acdd-5adf1221d616");
+            Guid id3 = Guid.Parse("8253c49f-f558-4f58-bfdb-f3c5a645cd0e");
             
             var photos = new Photo[]
             {
-                new Photo{ID=id1, BugID=1000, ObservationID=1},
-                new Photo{ID=id2, BugID=1001, ObservationID=2},
-                new Photo{ID=id3, BugID=1001, ObservationID=3}
+                new Photo{ID=id1, BugID=1001, ObservationID=2000},
+                new Photo{ID=id2, BugID=1001, ObservationID=2002},
+                new Photo{ID=id3, BugID=1000, ObservationID=2001}
             };
-            foreach (Photo e in photos)
+            foreach (Photo p in photos)
             {
-                context.Photos.Add(e);
+                context.Photos.Add(p);
             }
             context.SaveChanges();
         }
