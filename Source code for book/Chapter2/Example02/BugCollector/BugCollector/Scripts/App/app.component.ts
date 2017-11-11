5425e7d7c1d80ae4f7b42e7b16af42c9f208c21a
Tim Robinson <timrobinson41@gmail.com>
2017-11-10 19:17:41 -0800

    VS Integration and upgrade to Angular 2
    Integrated Angular 2 with VS (Ch. 2 Ex. 1). Got code from Ch. 1 Ex. 1 working in VS (Ch. 2 Ex. 2).

---
+++ Source code for book/Chapter2/Example02/BugCollector/BugCollector/Scripts/App/app.component.ts
@@ -0,0 +1,6 @@
+﻿import { Component } from '@angular/core';
+@Component({
+    selector: 'bug-collector',
+    template: '<bug-dropdown></bug-dropdown>'
+})
+export class AppComponent { }
---
+++ Source code for book/Chapter2/Example02/BugCollector/BugCollector/Scripts/App/app.module.ts
@@ -0,0 +1,11 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { BugDropdown } from './bugDropdown.component'; 
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [AppComponent, BugDropdown],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
